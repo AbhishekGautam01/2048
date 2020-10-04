92ae79a0aea0bcb44e4b28bdbafd4d676c0edd72
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-10-04 16:07:09 +0530

    Added Swipe Up , Swipe Down functionality Added Function to check win

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gridDisplay = document.querySelector('.grid');
     const scoreDisplay = document.querySelector('#score');
-    const resultDisplay = document.getElementById("result");
+    const resultDisplay = document.getElementById('result');
     const widht = 4;
     let squares = [];
 
@@ -71,6 +71,44 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //swipe dowm
+    function moveDown(){
+        for(let i=0; i < widht; i++){
+            let totalOne = squares[i].innerHTML;
+            let totalTwo = squares[i+widht].innerHTML;
+            let totalThree = squares[i+(widht*2)].innerHTML;
+            let totalFour = squares[i+(widht*3)].innerHTML;
+            let coulmn = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+            let filteredCoumn = coulmn.filter(num => num);
+            let missing = widht - filteredCoumn.length;
+            let zeros = Array(missing).fill(0);
+            let newColumn = zeros.concat(filteredCoumn);
+            squares[i].innerHTML = newColumn[0];
+            squares[i + (widht)].innerHTML = newColumn[1];
+            squares[i + (2*widht)].innerHTML = newColumn[2];
+            squares[i + (3*widht)].innerHTML = newColumn[3];
+        }
+    }
+
+     //swipe up
+     function moveUp(){
+        for(let i=0; i < widht; i++){
+            let totalOne = squares[i].innerHTML;
+            let totalTwo = squares[i+widht].innerHTML;
+            let totalThree = squares[i+(widht*2)].innerHTML;
+            let totalFour = squares[i+(widht*3)].innerHTML;
+            let coulmn = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+            let filteredCoumn = coulmn.filter(num => num);
+            let missing = widht - filteredCoumn.length;
+            let zeros = Array(missing).fill(0);
+            let newColumn = filteredCoumn.concat(zeros);
+            squares[i].innerHTML = newColumn[0];
+            squares[i + (widht)].innerHTML = newColumn[1];
+            squares[i + (2*widht)].innerHTML = newColumn[2];
+            squares[i + (3*widht)].innerHTML = newColumn[3];
+        }
+    }
+
     //combine
     function combineRows(){
         for(let i=0; i < (widht*widht)-1; i++) {
@@ -80,12 +118,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares[i+1].innerHTML = 0;
             }
         }
+        CheckWin();
+    }
+
+    function combineColumns(){
+        for(let i=0; i < (widht*widht)-widht; i++) {
+            if(squares[i].innerHTML === squares[i+widht].innerHTML){
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+widht].innerHTML);
+                squares[i].innerHTML = combinedTotal;
+                squares[i+widht].innerHTML = 0;
+            }
+        }
+        CheckWin();
     }
 
     //asign keycodes
     function control(e){
         if(e.keyCode === 39){
             keyRight();
+        } else if(e.keyCode === 37){
+            keyLeft();
+        } else if(e.keyCode === 38){
+            KeyUp();
+        } else if(e.keyCode === 40){
+            keyDown();
         }
     }
 
@@ -98,7 +154,36 @@ document.addEventListener('DOMContentLoaded', () => {
         generate();
     }
 
+    function keyLeft(){
+        moveLeft();
+        combineRows();
+        moveLeft();
+        generate();
+    }
 
+    function keyDown(){
+        moveDown();
+        combineColumns();
+        moveDown();
+        generate();
+    }
+
+    function KeyUp(){
+        moveUp();
+        combineColumns();
+        moveUp();
+        generate();
+    }
+
+    //check for win 
+    function CheckWin(){
+        for(let i =0; i < squares.length; i++){
+            if(squares[i].innerHTML == 2048){
+                resultDisplay.innerHTML = 'You Win';
+                document.removeEventListener('keyup', control);
+            }
+        }
+    }
 
 
     
